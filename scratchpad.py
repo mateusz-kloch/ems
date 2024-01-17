c36d94389f85c58b4efda8355a817f94803469cd
mateusz666x <mateusz.kloch@o2.pl>
2024-01-17 11:47:55 +0100

    some notes

--- scratchpad.py
+++ scratchpad.py
@@ -1,5 +1,1 @@
-# Changed name for expense class; "MyExpense" -> "UserExpense".
-
-#Changed names for functions; ("import_from_csv", "export_to_csv") -> ("import-csv", "export-csv").
-
-# Changed names for cli subcommands; ("import-csv", "export-csv") -> ("import-from", "export-to").
+# Changed name for: expense class "MyExpense" -> "UserExpense", functions ("import_from_csv", "export_to_csv") -> ("import-csv", "export-csv"), cli subcommands ("import-csv", "export-csv") -> ("import-from", "export-to").
